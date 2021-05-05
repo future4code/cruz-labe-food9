6edaf4629418d2642d72bedd407d025fb8c84d61
Elber-Sousa <elbe995@gmail.com>
2021-05-05 16:28:45 -0300

    iniciando resturantdetails

---
+++ src/components/CardScrollCategory/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledTabPanel = styled.div`
+    width: 360px;
+`;
---
+++ src/pages/RestaurantDetailPage/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const TextContainerHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  color: black;
+  padding: 10px;
+  font-weight: 500;
+  margin-bottom: 5px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+  height: 64px;
+`;
+
+export const StyledImg = styled.img`
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  width: 100%;
+  height: 120px;
+  object-fit: cover;
+`;
